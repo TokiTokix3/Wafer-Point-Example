0a3570021da86147b77c6ee9510655d74fba5283
Toki <36805172+TokiTokix3@users.noreply.github.com>
2023-02-17 12:04:37 -0800

    Initial commit for project
    Initial commit

---
+++ Wafer Point Application/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Wafer_Point_Application
+{
+    public partial class MainWindow : Window
+    {
+        List<Ellipse> activePlot = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void confirm_Click(object sender, RoutedEventArgs e)
+        {
+            PointCalculator calc = new PointCalculator();
+            deletePlot(this.activePlot);
+            try
+            {
+                int rings = calc.validateInput(int.Parse(inputBox.Text));
+                if (rings < 0)
+                {
+                    underText.Text = "Invalid number. Entry must satisfy (8n+1)";
+                    resultPoints.Text = "";
+                }
+                    
+                else
+                {
+                    underText.Text = "Rings generated: " + rings.ToString();
+                    List<Point> points = calc.generatePoints(rings);
+                    this.activePlot = plotList(points);
+                    resultPoints.Text = calc.printList(points);
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                underText.Text = "Invalid input";
+                resultPoints.Text = "";
+            }
+            
+        }
+
+        private List<Ellipse> plotList(List<Point> pointList)
+        {
+            List<Ellipse> plotPoints = new List<Ellipse>();
+            foreach (Point point in pointList)
+            {
+                Ellipse newEllipse = new Ellipse
+                {
+                    Width = 7,
+                    Height = 7,
+                    Fill = Brushes.Red,
+                    Stroke = Brushes.Red
+                };
+                plotPoints.Add(newEllipse);
+                Canvas.SetLeft(newEllipse, 576.5+point.x);
+                Canvas.SetTop(newEllipse, 203.5+point.y);
+                myCanvas.Children.Add(newEllipse);
+            }
+            return plotPoints;  
+
+        }
+
+        private void deletePlot(List<Ellipse> activePlot)
+        {
+            if(activePlot != null)
+            {
+                foreach (Ellipse plot in activePlot)
+                {
+                    myCanvas.Children.Remove(plot);
+                }
+            }
+
+        }
+    }
+}
---
+++ Wafer Point Application/PointCalculator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Wafer_Point_Application
+{
+    internal class Point
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    internal class PointCalculator
+    {
+        //number of slices = 8
+        //first point always at [0,0]
+
+        public int validateInput(int input)
+        {
+            if ((input - 1) % 8 != 0 || input < 1)
+                return -1;
+            else
+                return ((input - 1) / 8);
+        }
+
+        //input validation is done outside this function, takes in number of full rings to generate
+        public List<Point> generatePoints(int rings)
+        {
+            List<Point> pointList = new List<Point>();
+            pointList.Add(new Point(0, 0));
+            double ringStep = (150 / ((double)rings + 1)); //Distance between points on the slice
+            double distance = ringStep;
+
+            for (int i = 0; i < rings; i++)
+            {
+                double angle = Math.PI / 4;
+                for (int j = 0; j < 8; j++)
+                {
+                    double x = distance * Math.Sin(angle);
+                    double y = distance * Math.Cos(angle);
+                    pointList.Add(new Point(x, y));
+                    angle += Math.PI / 4;
+                }
+                distance += ringStep;
+            }
+            return pointList;
+        }
+
+        public string printList(List<Point> pointList)
+        {
+            string pointString = "";
+            int i = 0;
+            foreach (Point point in pointList)
+            {
+                pointString += "[" + Math.Round(point.x,2) + ", " + Math.Round(point.y, 2) + "] ";
+                if (i % 4 == 0)
+                {
+                    pointString += "\n";
+                    if (i % 8 == 0)
+                        pointString += "\n";
+                }
+                    
+
+                i++;
+            }
+            return pointString;
+        }
+    }
+}
